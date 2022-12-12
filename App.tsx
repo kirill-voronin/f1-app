f4ca87785b607887b9ff22ef2921bd9f2c01ead6
kirill-voronin <58369596+kirill-voronin@users.noreply.github.com>
2022-12-12 12:53:06 +0300

    нижнее работает только на основных экранах

--- App.tsx
+++ App.tsx
@@ -1,34 +1,12 @@
-import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
-import { NavigationContainer } from "@react-navigation/native";
-import PilotsStanding from "./screens/pilots-standing";
-import MenuIcons from "./icons/menu-icons";
 import { colors } from "./style/colors";
 import { StatusBar } from "react-native";
-import ConstructorsStanding from "./screens/commands-standing";
-import CalendarStack from "./screens/calendar/calendar-stack";
-import StatisticsStack from "./screens/statistics/statistics-stack";
-
-const MaterialTabs = createMaterialBottomTabNavigator();
+import Navigation from "./screens/navigation";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <>
+      <Navigation />
       <StatusBar animated={true} backgroundColor={colors.primary} />
-      <MaterialTabs.Navigator
-        barStyle={{ backgroundColor: colors.secondary }}
-        activeColor={colors.primary}
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ color }) => {
-            return <MenuIcons name={route.name} color={color} />;
-          },
-          tabBarActiveTintColor: "#EE191F",
-          tabBarInactiveTintColor: "#FFFFFF",
-        })}>
-        <MaterialTabs.Screen name="Календарь" component={CalendarStack} />
-        <MaterialTabs.Screen name="Пилоты" component={PilotsStanding} />
-        <MaterialTabs.Screen name="Команды" component={ConstructorsStanding} />
-        <MaterialTabs.Screen name="Статистика" component={StatisticsStack} />
-      </MaterialTabs.Navigator>
-    </NavigationContainer>
+    </>
   );
 }
