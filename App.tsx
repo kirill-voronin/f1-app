cff0b945c229181d8e24a2dc42939a524b8f6f43
kirill-voronin <58369596+kirill-voronin@users.noreply.github.com>
2023-02-10 16:42:42 +0300

    Получение токена для простых PUSH-уведомлений

--- App.tsx
+++ App.tsx
@@ -1,8 +1,56 @@
 import { colors } from "./style/colors";
-import { StatusBar } from "react-native";
+import { Platform, StatusBar, PermissionsAndroid, Alert } from "react-native";
 import Navigation from "./screens/navigation";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import { useEffect, useState } from "react";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  const [expoPushToken, setExpoPushToken] = useState<string>();
+
+  async function registerForPushNotificationsAsync() {
+    let token;
+
+    // Создаем канал уведомлений
+    if (Platform.OS === "android") {
+      await Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    if (Device.isDevice) {
+      // запрашиваем разрешение на отправку уведомлений
+      const { status: existingStatus } = await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        return;
+      }
+      // получаем токен устройства
+      token = (await Notifications.getExpoPushTokenAsync()).data;
+    }
+
+    return token;
+  }
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => setExpoPushToken(token));
+  });
+
   return (
     <>
       <Navigation />
