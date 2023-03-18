cffcc7759d3cafcc78dff6f812093272b92c105d
kirill-voronin <kirill223v@gmail.com>
2023-03-18 13:57:06 +0300

    feat(calendar): Количество дней/часов до гонки

---
+++ src/functions/getTimeToRace.ts
@@ -0,0 +1,62 @@
+export function getTimeToRace(date?: string, time?: string): string {
+  if (!date && !time) {
+    return "";
+  }
+  const currentDate = new Date();
+
+  let timeToRace = Math.floor(
+    Math.abs(new Date(`${date}T${time}`).getTime() - currentDate.getTime()) /
+      (1000 * 60 * 60 * 24) // ms->ss->mm->hh->dd
+  );
+
+  if (timeToRace >= 1) {
+    return `До гонки ${timeToRace} ${getCorrectDayName(timeToRace)}`;
+  }
+
+  timeToRace = Math.floor(
+    Math.abs(new Date(`${date}T${time}`).getTime() - currentDate.getTime()) /
+      (1000 * 60 * 60) // ms->ss->mm->hh
+  );
+
+  return `До гонки ${timeToRace} ${getCorrectHourName(timeToRace)}`;
+}
+
+const getCorrectHourName = (hour: number) => {
+  switch (hour % 10) {
+    case 1:
+      return "час";
+    case 2:
+    case 3:
+    case 4:
+      return "часа";
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 0:
+      return "5";
+    default:
+      return "";
+  }
+};
+
+const getCorrectDayName = (day: number) => {
+  switch (day % 10) {
+    case 1:
+      return "день";
+    case 2:
+    case 3:
+    case 4:
+      return "дня";
+    case 5:
+    case 6:
+    case 7:
+    case 8:
+    case 9:
+    case 0:
+      return "дней";
+    default:
+      return "";
+  }
+};
